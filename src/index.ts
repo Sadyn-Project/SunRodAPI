14492b6e0ddb807e1aa21e16e0e37ab0b169944b
GabrieleYT9768 <77356614+GabrieleYT9768@users.noreply.github.com>
2022-07-06 19:14:35 +0200

    Fixing TypeScript compiling problems (1)

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,6 @@ const checkType = (input: { name: string, type: string, value: any }[]) => {
 	for (const element of input) if (typeof element.value !== element.type) throw new TypeError(`"${element.name}" property was expected to be ${element.type == 'object' ? 'an' : 'a'} ${element.type}, but received ${typeof element.value == 'object' ? 'an' : 'a'} ${typeof element.value}. Follow documentation for more details.`);
 };
 
-
 class SunRodAPI {
 	token: string;
 
