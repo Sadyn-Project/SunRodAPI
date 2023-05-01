50d801781fc3bf215cf7a42f1345804710b7e050
GabrieleYT9768 <77356614+GabrieleYT9768@users.noreply.github.com>
2023-05-01 22:49:53 +0200

    Fixed endpoint
    If there would be any error with the API request, the code would throw an axios error.
    Now, the reason appears.

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -3,31 +3,46 @@ import axios from 'axios';
 const endpoint = {
 	hostname: 'sunrod.it',
 	get: async (dir: string, token: string): Promise<any> => {
-		const res = await axios.get(`https://${endpoint.hostname}/api/v1/${dir}`, { headers: {
-			'Authorization': `Bearer ${token}`,
-			'Content-Type': 'application/json',
-			'Accept': 'application/json',
-		} });
-		if (res.data === 'Internal Server Error') throw new Error('SunRod had an Internal Error. Check availability on https://status.sadyn.it');
-		else return res;
+		try {
+			const res = await axios.get(`https://${endpoint.hostname}/api/v1/${dir}`, { headers: {
+				'Authorization': `Bearer ${token}`,
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+			} });
+			if (res.data === 'Internal Server Error') throw new Error('SunRod had an Internal Error. Check availability on https://status.sadyn.it');
+			else if (res.data === 'Unauthorized') throw new Error('You tried to use the SunRod API, but the token is invalid.');
+			else return res;
+		} catch (err) {
+			throw new Error('SunRod is currently unreachable. Check availability on https://status.sadyn.it');
+		}
 	},
 	post: async (dir: string, body: any, token: string): Promise<any> => {
-		const res = await axios.post(`https://${endpoint.hostname}/api/v1/${dir}`, body, { headers: {
-			'Authorization': `Bearer ${token}`,
-			'Content-Type': 'application/json',
-			'Accept': 'application/json',
-		} });
-		if (res.data === 'Internal Server Error') throw new Error('SunRod had an Internal Error. Check availability on https://status.sadyn.it');
-		else return res;
+		try {
+			const res = await axios.post(`https://${endpoint.hostname}/api/v1/${dir}`, body, { headers: {
+				'Authorization': `Bearer ${token}`,
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+			} });
+			if (res.data === 'Internal Server Error') throw new Error('SunRod had an Internal Error. Check availability on https://status.sadyn.it');
+			else if (res.data === 'Unauthorized') throw new Error('You tried to use the SunRod API, but the token is invalid.');
+			else return res;
+		} catch (err) {
+			throw new Error('SunRod is currently unreachable. Check availability on https://status.sadyn.it');
+		}
 	},
 	delete: async (dir: string, token: string): Promise<any> => {
-		const res = await axios.delete(`https://${endpoint.hostname}/api/v1/${dir}`, { headers: {
-			'Authorization': `Bearer ${token}`,
-			'Content-Type': 'application/json',
-			'Accept': 'application/json',
-		} });
-		if (res.data === 'Internal Server Error') throw new Error('SunRod had an Internal Error. Check availability on https://status.sadyn.it');
-		else return res;
+		try {
+			const res = await axios.delete(`https://${endpoint.hostname}/api/v1/${dir}`, { headers: {
+				'Authorization': `Bearer ${token}`,
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+			} });
+			if (res.data === 'Internal Server Error') throw new Error('SunRod had an Internal Error. Check availability on https://status.sadyn.it');
+			else if (res.data === 'Unauthorized') throw new Error('You tried to use the SunRod API, but the token is invalid.');
+			else return res;
+		} catch (err) {
+			throw new Error('SunRod is currently unreachable. Check availability on https://status.sadyn.it');
+		}
 	}
 }
 
